998ca3fbf2f25f5e9560ce108228c4f528ef44e2
erickjcss <151551835+erickjcss@users.noreply.github.com>
2025-11-17 09:53:39 -0500

    Update settings.py

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -123,6 +123,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://pizzeriafront.onrender.com",
 ]
 
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
